a5af7a567581343d0f54e79b53a14291123b1b23
sabryassaf <sabry.assaf12@gmail.com>
2024-08-14 15:38:35 +0300

    need to implement the 10 100 1000 episodes in Wet 1

--- hw3/MDP/main.py
+++ hw3/MDP/main.py
@@ -1,7 +1,7 @@
-
 from mdp_rl_implementation import value_iteration, get_policy, policy_evaluation, policy_iteration, adp_algorithm
 from mdp import MDP
 from simulator import Simulator
+
 
 def example_driver():
     """
@@ -14,7 +14,7 @@ def example_driver():
     print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
     print("@@@@@@ The board and rewards @@@@@@")
     print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
-    
+
     mdp.print_rewards()
 
     print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
@@ -33,7 +33,6 @@ def example_driver():
     print("\nFinal policy:")
     policy = get_policy(mdp, U_new)
     mdp.print_policy(policy)
-
 
     print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
     print("@@@@@@@@@ Policy iteration @@@@@@@@")
@@ -54,25 +53,34 @@ def example_driver():
     mdp.print_policy(policy_new)
 
     print("Done!")
-    
 
 
 def adp_example_driver():
-
     sim = Simulator()
 
-    
-    reward_matrix, transition_probabilities = adp_algorithm(sim,num_episodes=10)
-  
+    reward_matrix, transition_probabilities = adp_algorithm(sim, num_episodes=10)
+
     print("Reward Matrix:")
     print(reward_matrix)
-    
+
     print("\n Transition Probabilities:")
     for action, probs in transition_probabilities.items():
         print(f"{action}: {probs}")
 
-    
+def adp_extra_examples():
+    sim = Simulator()
+    num_episodes_list = [10, 100, 1000]
+    for num_episodes in num_episodes_list:
+        reward_matrix, transition_probabilities = adp_algorithm(sim, num_episodes=num_episodes)
+        mdp = MDP.load_mdp(board=reward_matrix, transition_function=transition_probabilities)
+        policy = [['UP', 'UP', 'UP', 0],
+                  ['UP', 'WALL', 'UP', 0],
+                  ['UP', 'UP', 'UP', 'UP']]
+        policy_new = policy_iteration(mdp, policy)
+        print(f"\nPolicy after {num_episodes} episodes:")
+
 if __name__ == '__main__':
     # run our example
-    example_driver()
-    adp_example_driver()
+    # example_driver()
+    # adp_example_driver()
+    adp_extra_examples()
